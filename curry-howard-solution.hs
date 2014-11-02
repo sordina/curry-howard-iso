11f8ea8407943790a3946e5ce2176de5d2d8c8a0
Lyndon Maydwell <lyndon@sordina.net>
2014-11-02 20:35:53 +1100

    Haskell and Idris solutions

---
+++ curry-howard-solution.hs
@@ -0,0 +1,152 @@
+{-# LANGUAGE TypeOperators #-}
+data a :/\: b = a :/\: b
+data a :\/: b = L a | R b
+
+data False
+type Not a = a -> False
+
+data a :<->: b = Bi {
+  forward :: (a -> b),
+  backward :: (b -> a)
+}
+
+lhs :: a :/\: b -> a
+rhs :: a :/\: b -> b
+
+lhs (p :/\: _) = p
+rhs (_ :/\: q) = q
+
+and_commute :: a :/\: b -> b :/\: a
+and_commute (x :/\: y) = (y :/\: x)
+
+or_commute :: a :\/: b -> b :\/: a
+or_commute (L x) = (R x)
+or_commute (R x) = (L x)
+
+and_assoc :: a :/\: (b :/\: c) -> (a :/\: b) :/\: c
+and_assoc (x :/\: (y :/\: z)) = (x :/\: y) :/\: z
+
+or_assoc :: a :\/: (b :\/: c) -> (a :\/: b) :\/: c
+or_assoc (L x)     = L (L x)
+or_assoc (R (L y)) = L (R y)
+or_assoc (R (R z)) = R z
+
+modus_ponens :: a -> (a -> b) -> b
+modus_ponens = flip id
+
+and_idempotent :: a :/\: a -> a
+and_idempotent = lhs
+
+or_idempotent :: a :\/: a -> a
+or_idempotent (L x) = x
+or_idempotent (R x) = x
+
+and_distributes :: a :/\: (b :\/: c) -> (a :/\: b) :\/: (a :/\: c)
+and_distributes (x :/\: L y) = L (x :/\: y)
+and_distributes (x :/\: R z) = R (x :/\: z)
+
+or_distributes :: a :\/: (b :/\: c) -> (a :\/: b) :/\: (a :\/: c)
+or_distributes (L x)          = L x :/\: L x
+or_distributes (R (y :/\: z)) = R y :/\: R z
+
+contrapositive :: (a -> b) -> (Not b -> Not a)
+contrapositive = undefined
+
+demorgan1 :: Not a :\/: Not b -> Not (a :/\: b)
+demorgan1 = undefined
+
+demorgan2a :: Not a :/\: Not b -> Not (a :\/: b)
+demorgan2a = undefined
+
+demorgan2b :: Not (a :\/: b) -> Not a :/\: Not b
+demorgan2b = undefined
+
+demorgan2 :: Not a :/\: Not b :<->: Not (a :\/: b)
+demorgan2 = undefined
+
+data Scottish    = Scottish
+data RedSocks    = RedSocks
+data WearKilt    = WearKilt
+data Married     = Married
+data GoOutSunday = GoOutSunday
+
+no_true_scottsman ::
+  (Not Scottish -> RedSocks)          -> -- rule 1
+  (WearKilt :\/: Not RedSocks)        -> -- rule 2
+  (Married -> Not GoOutSunday)        -> -- rule 3
+  (Scottish :<->: GoOutSunday)        -> -- rule 4
+  (WearKilt -> Scottish :/\: Married) -> -- rule 5
+  (Scottish -> WearKilt)              -> -- rule 6
+  False
+
+no_true_scottsman
+  rule1
+  rule2
+  rule3
+  rule4
+  rule5
+  rule6 = let
+    lemma1 :: Scottish -> Married
+    lemma1 = undefined
+
+    lemma2 :: Scottish -> Not GoOutSunday
+    lemma2 = undefined
+
+    lemma3 :: Scottish -> GoOutSunday
+    lemma3 = undefined
+
+    lemma4 :: Not Scottish
+    lemma4 scottish = undefined
+
+    lemma5 :: RedSocks
+    lemma5 = undefined
+
+    lemma6 :: WearKilt :\/: False
+    lemma6 = undefined
+
+    lemma7 :: Not WearKilt -> False
+    lemma7 = undefined
+
+    lemma8 :: Not Scottish -> Not WearKilt
+    lemma8 = undefined
+
+    lemma9 :: Not Scottish -> False
+    lemma9 = undefined
+
+    in undefined
+
+
+rule1 :: Not Scottish -> RedSocks
+rule2 :: WearKilt :\/: Not RedSocks
+rule3 :: Married -> Not GoOutSunday
+rule4 :: Scottish :<->: GoOutSunday
+rule5 :: WearKilt -> Scottish :/\: Married
+rule6 :: Scottish -> WearKilt
+
+lemma1 :: Scottish -> Married
+lemma2 :: Scottish -> Not GoOutSunday
+lemma3 :: Scottish -> GoOutSunday
+lemma4 :: Not Scottish
+lemma5 :: RedSocks
+lemma6 :: WearKilt :\/: False
+lemma7 :: Not WearKilt -> False
+lemma8 :: Not Scottish -> Not WearKilt
+lemma9 :: Not Scottish -> False
+
+
+rule1 = undefined
+rule2 = undefined
+rule3 = undefined
+rule4 = undefined
+rule5 = undefined
+rule6 = undefined
+
+lemma1 = undefined
+lemma2 = undefined
+lemma3 = undefined
+lemma4 = undefined
+lemma5 = undefined
+lemma6 = undefined
+lemma7 = undefined
+lemma8 = undefined
+lemma9 = undefined
---
+++ curry-howard-solutions.idr
@@ -0,0 +1,86 @@
+module curry_howard_solutions
+
+-- Note: Spoilers! Solutions Included!
+
+%default total
+
+infixl 4 /\
+infixl 4 :/\
+
+data (:/\) : Type -> Type -> Type where
+      (/\) : {a : Type} -> {b : Type} -> a -> b -> a :/\ b
+
+infixl 3 :\/
+
+data (:\/) : Type -> Type -> Type where
+     L     : {a : Type} -> {b : Type} -> a -> a :\/ b
+     R     : {a : Type} -> {b : Type} -> b -> a :\/ b
+
+-- data Void
+-- Not : Type
+-- Not a = a -> Void
+
+infixr 2 :<->
+
+record (:<->) : Type -> Type -> Type where
+      (<->)   :  {a : Type}
+              -> {b : Type}
+              -> (forward  : a -> b)
+              -> (backward : b -> a)
+              -> a :<-> b
+
+lhs : {a : Type} -> {b : Type} -> a :/\ b -> a
+lhs (x /\ _) = x
+
+rhs : {a : Type} -> {b : Type} -> a :/\ b -> b
+rhs (_ /\ y) = y
+
+and_commutes : {a : Type} -> {b : Type} -> a :/\ b -> b :/\ a
+and_commutes (x /\ y) = y /\ x
+
+and_associates : (a :/\ b) :/\ c -> a :/\ (b :/\ c)
+and_associates ((x /\ z) /\ y) = x /\ (z /\ y)
+
+or_commutes : a :\/ b-> b :\/ a
+or_commutes (L x) = R x
+or_commutes (R x) = L x
+
+or_associates : (a :\/ b) :\/ c -> a :\/ (b :\/ c)
+or_associates (L (L x)) = L x
+or_associates (L (R x)) = R (L x)
+or_associates    (R x)  = R (R x)
+
+modus_ponens : a -> (a -> b) -> b
+modus_ponens v f = f v
+
+and_indempotent : a :/\ b -> a
+and_indempotent = lhs
+
+or_indempotent : a :\/ a -> a
+or_indempotent (L x) = x
+or_indempotent (R x) = x
+
+and_distributes : a :/\ (b :\/ c) -> (a :/\ b) :\/ (a :/\ c)
+and_distributes (x /\ (L y)) = L (x /\ y)
+and_distributes (x /\ (R y)) = R (x /\ y)
+
+or_distributes : a :\/ (b :/\ c) -> (a :\/ b) :/\ (a :\/ c)
+or_distributes (L x)        = L x /\ L x
+or_distributes (R (x /\ y)) = R x /\ R y
+
+contrapositive : (a -> b) -> (Not b -> Not a)
+contrapositive f pbf = pbf . f
+
+demorgan_1 : Not a :\/ Not b -> Not (a :/\ b)
+demorgan_1 (L x) = x . lhs
+demorgan_1 (R x) = x . rhs
+
+demorgan_2a : Not a :/\ Not b -> Not (a :\/ b)
+demorgan_2a (x /\ y) (L f) = x f
+demorgan_2a (x /\ y) (R f) = y f
+
+demorgan_2b : Not (a :\/ b) -> Not a :/\ Not b
+demorgan_2b f = f . L /\ f . R
+
+demorgan2 : Not a :/\ Not b :<-> Not (a :\/ b)
+demorgan2 = demorgan_2a <-> demorgan_2b
